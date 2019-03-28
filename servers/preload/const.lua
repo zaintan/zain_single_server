8176cded1f66cda280dd3a9211523d510af164e0
zain_win <tzytclzxh@qq.com>
2019-03-28 11:43:00 +0800

    -----------

--- servers/preload/const.lua
+++ servers/preload/const.lua
@@ -6,5 +6,7 @@ M.PlayerStatus = require("constants.PlayerStatus")
 M.GameRule     = require("constants.GameRule")
 M.GameAction   = require("constants.GameAction")
 M.LoginType    = require("constants.LoginType")
+M.GameHandler  = require("constants.GameHandler")
+M.GameOverType  = require("constants.GameOverType")
 
 return M
