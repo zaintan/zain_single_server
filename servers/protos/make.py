310c87db92b67f284cc06a8b0cdd1b8a92057058
zain_win <tzytclzxh@qq.com>
2019-04-12 14:05:07 +0800

    ---编译proto  python脚本辅助

---
+++ servers/protos/make.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*- 
+import os
+import sys
+import string
+
+path = os.path.join(os.getcwd(), "proto")
+
+def excuteCMD(cmd):
+	print("excute cmd:" + cmd)
+	os.system(cmd)
+
+def listdir(path,list_name):  # 传入存储的list
+    for file in os.listdir(path):
+        file_path = os.path.join(path, file)
+        if not os.path.isdir(file_path):
+        	if file_path.find('.proto')!=-1:
+        		list_name.append(file)
+
+l = []
+listdir(path, l)
+
+for name in l:
+	cmd = "protoc -o ./ ./proto/" + name
+	excuteCMD(cmd)
