df09b7a28a46a9545b920e7ca7d34c97cc1f4d9d
zain_ubantu <tzytclzxh@qq.com>
2019-05-12 01:17:54 +0800

    -------

--- servers/server_alloc/start.sh
+++ servers/server_alloc/start.sh
@@ -8,7 +8,8 @@
 ### so we can use getenv in skynet
 export ServerIndex=$1
 
-export StartTime=`date +%Y%m%d_%H%M%S`
+#export StartTime=`date +%Y%m%d_%H%M%S`
+export StartTime=`date +%Y%m%d_%H`
 
 ### so we can distinguish different skynet processes
 ../skynet/skynet server_alloc/config.lua $ServerIndex
